dcdb5fd284bde5a175432b76b7d4ffe6a29c4c0f
coutar-a <AGCoutarel@gmail.com>
2015-04-13 15:16:21 +0200

    wow

--- src/step1.ml
+++ src/step1.ml
@@ -1,1 +1,5 @@
-print_endline("step1");
+let arglist = Array.to_list Sys.argv;;
+
+if List.length arglist != 3 then (print_endline("invalid argument number") ; exit 0) ;;
+
+if List.nth arglist 1 = List.nth arglist 2 then print_endline("yéé carré") else (print_endline("invalid arguments") ; exit 0) ;;

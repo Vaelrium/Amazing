82826ab4a8e1aa5f3c26112f843d9a1ccf66200e
durand_u <durand_u@epitech.eu>
2015-04-13 15:27:00 +0200

    lol

---
+++ src/Modules.ml
@@ -0,0 +1,17 @@
+
+module	Gate =
+  struct
+    let doors = 0 :: 0 :: 0 :: 0 :: []
+  end
+
+module	Case =
+  struct
+    let x = 0
+    let y = 0
+    Gate gate
+  end
+
+module	Maze =
+  struct
+    let cases = []
+  end

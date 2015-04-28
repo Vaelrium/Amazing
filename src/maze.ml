736cdff8202509217c643a5fc585d69eb38feca8
durand_u <durand_u@epitech.eu>
2015-04-28 11:45:23 +0200

    lol

--- src/maze.ml
+++ src/maze.ml
@@ -1,5 +1,6 @@
 
 
+
 module type MAZE =
   sig
 
--- src/step1.ml
+++ src/step1.ml
@@ -9,4 +9,13 @@ else (print_endline("invalid arguments") ; exit 0) ;;
 
 let maze = Maze.Maze.create_maze 10;;
 
-Maze.Maze.desc_maze maze;
+Maze.Maze.desc_maze maze;;
+
+let cases = Maze.Maze.get_maze maze;;
+
+if (Case.Case.get_x cases.(1).(5)) = 5 then Printf.printf "ESCALOPE\n";;
+
+Random.self_init();;
+
+let butthole = Random.int 100;;
+print_int butthole;;
